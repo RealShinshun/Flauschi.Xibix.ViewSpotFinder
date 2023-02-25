0d3605f936f404ad4e89a7b75e3ee31eef3fab45
flauschi <c.gmach@zenamo-software.de>
2023-02-25 18:20:59 +0100

    Add ViewSpotFinder to find view spots
     - add ViewSpotFinder to find all ViewSpots or limited by amount
     - add tests to verify serialization and view spot finding performance

---
+++ Flauschi.Xibix.ViewSpotFinder/Flauschi.Xibix.ViewSpotFinder.Tests/PerformanceTests.cs
@@ -0,0 +1,88 @@
+using Flauschi.Xibix.ViewSpotFinder.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Flauschi.Xibix.ViewSpotFinder.Tests
+{
+    [TestClass]
+    public class PerformanceTests
+    {
+        [TestMethod]
+        public void FindsViewSpots_OfMeshWith10kElements_InLessThan15Seconds()
+        {
+            var averageTimeTaken = MeasureAverage(
+                () =>
+                {
+                    var mesh = MeshData.FromFile(
+                        "./Files/mesh_x_sin_cos_10000[82][1][1][1][1][1][1].json");
+                    _ = new ViewSpotFinder().FindAll(mesh);
+                }, 10);
+
+            Assert.IsTrue(averageTimeTaken.TotalSeconds < 15);
+        }
+
+        [TestMethod]
+        public void FindsViewSpots_OfMeshWith10kElements_InLessThan1Second()
+        {
+            var averageTimeTaken = MeasureAverage(
+                () =>
+                {
+                    var mesh = MeshData.FromFile(
+                        "./Files/mesh_x_sin_cos_10000[82][1][1][1][1][1][1].json");
+                    _ = new ViewSpotFinder().FindAll(mesh);
+                }, 10);
+
+            Assert.IsTrue(averageTimeTaken.TotalSeconds < 1);
+        }
+
+        [TestMethod]
+        public void FindsViewSpots_OfMeshWith20kElements_IsLessThan3TimesHigherThanWith10k()
+        {
+            var averageTimeTakenFor10k = MeasureAverage(
+                () =>
+                {
+                    var mesh = MeshData.FromFile(
+                        "./Files/mesh_x_sin_cos_10000[82][1][1][1][1][1][1].json");
+                    _ = new ViewSpotFinder().FindAll(mesh);
+                }, 20);
+
+            var averageTimeTakenFor20k = MeasureAverage(
+                () =>
+                {
+                    var mesh = MeshData.FromFile(
+                        "./Files/mesh_x_sin_cos_20000[1][1][1][1][1][1].json");
+                    _ = new ViewSpotFinder().FindAll(mesh);
+                }, 20);
+
+            Assert.IsTrue(
+                averageTimeTakenFor20k < averageTimeTakenFor10k * 3,
+                $"10k: {averageTimeTakenFor10k.TotalMilliseconds}, 20k: {averageTimeTakenFor20k.TotalMilliseconds}");
+        }
+
+        private static TimeSpan MeasureAverage(
+            Action action,
+            int executions)
+        {
+            var stopwatch = new Stopwatch();
+            var measuredTimes = new List<TimeSpan>();
+
+            for (var i = 0; i < executions; i++)
+            {
+                stopwatch.Start();
+
+                action();
+
+                stopwatch.Stop();
+                measuredTimes.Add(stopwatch.Elapsed);
+
+                stopwatch.Reset();
+            }
+
+            return TimeSpan.FromMilliseconds(
+                measuredTimes.Average(x => x.TotalMilliseconds));
+        }
+    }
+}
---
+++ Flauschi.Xibix.ViewSpotFinder/Flauschi.Xibix.ViewSpotFinder/ViewSpotFinder.cs
@@ -0,0 +1,60 @@
+﻿using Flauschi.Xibix.ViewSpotFinder.Data;
+
+namespace Flauschi.Xibix.ViewSpotFinder
+{
+    public class ViewSpotFinder
+    {
+        public ViewSpotFinder()
+        { /* empty */ }
+
+        public ViewSpot[] FindAll(MeshData meshData)
+        {
+            var mesh = Mesh.FromMeshData(meshData);
+
+            return FindViewSpots(mesh.Elements.OrderByDescending(x => x.Value))
+                .ToArray();
+        }
+
+        public ViewSpot[] Find(MeshData meshData, int amount)
+        {
+            var mesh = Mesh.FromMeshData(meshData);
+
+            return FindViewSpots(mesh.Elements)
+                .Take(amount)
+                .ToArray();
+        }
+
+        private IEnumerable<ViewSpot> FindViewSpots(
+            IEnumerable<Element> elements)
+        {
+            var excludedElements = new HashSet<Element>();
+
+            foreach (var element in elements)
+            {
+                if (excludedElements.Contains(element))
+                    continue;
+
+                var neighboringElements = element.Nodes
+                    .SelectMany(x => x.Elements)
+                    .ToList();
+
+                var isLocalMaxima = neighboringElements
+                    .All(x => x.Value <= element.Value);
+
+                if (isLocalMaxima)
+                {
+                    yield return new ViewSpot
+                    {
+                        ElementId = element.Id,
+                        Value = element.Value
+                    };
+
+                    excludedElements.UnionWith(
+                        neighboringElements);
+                }
+
+                excludedElements.Add(element);
+            }
+        }
+    }
+}
